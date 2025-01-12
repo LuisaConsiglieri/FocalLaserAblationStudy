1c7784c2488e8eab42de15985021d64095802d68
Luisa Consiglieri <81486025+LuisaConsiglieri@users.noreply.github.com>
2025-01-12 17:01:32 +0000

    Create data_geometry2025.m
    This script stores the geometric parameters [mm].

---
+++ .github/workflows/data_geometry2025.m
@@ -0,0 +1,51 @@
+% This script stores the geometric parameters [mm].
+
+% radial parameters
+r_f = 0.3 ;  % radius of optical fiber
+r_i = 10 ; % radius of tumor inner = 1 cm
+r_fatty = 15 ; % surrounding healthy tissue
+r_o = r_i+ r_fatty ; % outer radius = 2.5 cm
+
+rr = linspace(0,r_o);
+count = 1;
+while rr(count) < r_i
+    count = count +1;
+end
+
+rr(2) = r_f; % matching value
+[rr(1) rr(2) rr(3)]; % validation
+
+ii = count; % 41 ok
+rr(ii) = r_i;
+[ rr(ii-1) rr(ii) rr(ii+1) ]; % validation
+
+% ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+% longitudinal parameters
+L0 = 0 ; % location of the focal point
+ell = 2*r_i ; % tumor length
+z_fatty = 5 ; % surrounding healthy tissue
+L = ell + z_fatty ; % total length = 2.5 cm
+
+% zz_end = ell; % TUMOR only
+zz_end = L;
+
+zz = linspace(L0,zz_end);
+count = 1;
+while zz(count) < 0
+    count = count +1;
+end
+
+i0 = count; % 1
+z0 = zz(count); % find(zz==0)
+while zz(count) < ell
+    count=count+1;
+end
+
+iell = count; % 81
+N_out = length(zz(iell:end));  % zz(iell) = 20.152
+zz(iell) = ell;
+
+z1 = zz(1 : iell-1);   %   z1(end) = 19.949 ;  length(z1) = 80
+z2 = linspace(ell, L, N_out);     % z2(1) = ell;  1x20
+
