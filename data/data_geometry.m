bb7ee411407ae69cb573794ddc9ca1076b4818c4
Luisa Consiglieri <81486025+LuisaConsiglieri@users.noreply.github.com>
2025-01-29 14:36:21 +0000

    Update data_geometry.m

--- data/data_geometry.m
+++ data/data_geometry.m
@@ -9,7 +9,7 @@ r_o = r_i + r_fatty ; % outer radius = 2.5 cm
 rr = linspace(0, r_o);
 count = 1;
 while rr(count) < r_i
-    count = count +1;
+    count = count + 1;
 end
 
 rr(2) = r_f; % matching value
@@ -28,7 +28,7 @@ L = ell + z_fatty ; % total length = 2.5 cm
 zz = linspace(L0, L);
 count = 1;
 while zz(count) < 0
-    count = count +1;
+    count = count + 1;
 end
 
 i0 = count; % 1
@@ -41,5 +41,5 @@ iell = count; % 81
 N_out = length(zz(iell:end));  % zz(iell) = 20.152
 zz(iell) = ell;
 
-z1 = zz(1 : iell-1);   %   z1(end) = 19.949 ;  length(z1) = 80
+z1 = zz(1:iell-1);   %   z1(end) = 19.949 ;  length(z1) = 80
 z2 = linspace(ell, L, N_out);     % z2(1) = ell;  1x20
