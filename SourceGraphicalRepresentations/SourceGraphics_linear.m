a7515465c312d901d47ecccfb6874656bfb1f50d
Luisa Consiglieri <81486025+LuisaConsiglieri@users.noreply.github.com>
2025-03-09 17:24:39 +0000

    Update SourceGraphics_linear.m

--- SourceGraphicalRepresentations/SourceGraphics_linear.m
+++ SourceGraphicalRepresentations/SourceGraphics_linear.m
@@ -3,19 +3,19 @@ function SourceGraphics_linear(z1, source1, z2, source2)
  
 % --------------------------Linear ------------------------
 ll = 1;  % 810nm and pw = 1
-plot(z1, source1(ll,1,:), 'b', z2, source2(ll,1,:), 'b')
+plot(z1, source1(ll, 1, :), 'b', z2, source2(ll, 1, :), 'b')
 hold on
  
 ll = 2;  % 980nm 
 pw = 1;
-plot(z1, source1(ll, pw,:), 'b--', z2, source2(ll, pw,:), 'b--')
+plot(z1, source1(ll, pw, :), 'b--', z2, source2(ll, pw, :), 'b--')
 hold on
 pw = 2;
-plot(z1, source1(ll, pw,:), 'r--', z2, source2(ll, pw,:), 'r--')
+plot(z1, source1(ll, pw, :), 'r--', z2, source2(ll, pw, :), 'r--')
 hold on
 
 ll = 3; % 1064nm, pw = 2
-plot(z1, source1(ll,2,:), 'r', z2, source2(ll,2,:), 'r')
+plot(z1, source1(ll, 2, :), 'r', z2, source2(ll, 2, :), 'r')
 
 xlabel('z, mm')
 ylabel('S, W/mm^3')
