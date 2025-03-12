f629f4b5d2827987c2a502ef97ebf16374bcdf3f
Luisa Consiglieri <81486025+LuisaConsiglieri@users.noreply.github.com>
2025-03-12 20:32:06 +0000

    Add files via upload

---
+++ SourceGraphicalRepresentations/SourceGraphics_semilogy.m
@@ -0,0 +1,27 @@
+function SourceGraphics_semilogy(z1, source1, z2, source2)
+% This function illustrates the graphical representations to the source S(r_f,z)
+
+% ------------------------------ semilogy ---------------------------------
+ll = 1;  % 810nm and pw = 1
+semilogy(z1, source1(ll,1,:), 'b', z2, source2(ll,1,:), 'b')
+hold on
+
+ll = 2; % 980nm
+pw = 1;
+semilogy(z1, source1(ll, pw,:), 'b--', z2, source2(ll, pw,:), 'b--')
+hold on
+pw = 2;
+semilogy(z1, source1(ll, pw,:), 'r--', z2, source2(ll, pw,:), 'r--')
+hold on
+
+ll = 3; % 1064nm, pw = 2
+semilogy(z1, source1(ll,2,:), 'r', z2, source2(ll,2,:), 'r')
+
+xlabel('z, mm')
+ylabel('S, W/mm^3')
+
+% ----------------------------------------------------------
+
+
+%axis ("tic(x)", "label");
+endfunction
