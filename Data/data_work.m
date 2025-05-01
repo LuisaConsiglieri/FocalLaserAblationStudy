7fb33362bc0ee93b698ffbf432ce476965f14412
Luisa Consiglieri <81486025+LuisaConsiglieri@users.noreply.github.com>
2025-05-01 14:33:53 +0100

    Update and rename data_work.m to data_work.m

--- Data/data_work.m
+++ Data/data_work.m
@@ -1,21 +1,18 @@
 % This script stores the working parameters.
 
 % ------------------------Appendix A.1: Particular solution -------------
-% Calculate parameters beta, time independent (S1= 0),
-% dependent on wavelength  lambda = 810, 980, 1064 nm
-%  3*mu_tr(:,j)  = 1/D^{(o)}
+% Calculate parameters beta, time independent (S1 = 0), dependent on wavelength  lambda = 810, 980, 1064 nm
 
-% beta < 0
-beta = zeros(3,4);
-for j = 1:2  % carcinoma breast (inner) + adipose breast (outer)
-  beta(:,j) = 3*mu_tr(:,j).*mu_a(:,j) - mu_t(:,1).^2 ;
-endfor
-for j = 3:4
-  beta(:,j) = 3*mu_tr(:,j).*mu_a(:,j) - mu_t(:,3).^2 ;
-endfor
+beta = zeros(3, 4);
+for j = 1:2 % breast carcinoma (inner) + adipose (outer)
+  beta(:, j) = 3*mu_tr(:,j).*mu_a(:,j) - mu_t(:,1).^2 ;
+end
+for j = 3:4 % prostate tumor (inner) + healthy (outer)
+  beta(:, j) = 3*mu_tr(:,j).*mu_a(:,j) - mu_t(:,3).^2 ;
+end
 
 % -------------- j_tissue = 1:2 -------------------------------
-% for  INNER (tumor) j = 1, 3 % carcinoma breast - prostate tumor
+% for  INNER (tumor) j = 1, 3 % breast carcinoma - prostate tumor
 % S1 = zeros(3, 2);
 S1(:, 1) = D(:,1).*mu_t(:,1).^2 - mu_a(:,1)  ;
 S1(:, 2) = D(:,3).*mu_t(:,3).^2 - mu_a(:,3)  ;
@@ -42,7 +39,3 @@ eta2_prostate = sqrt(eta0(:,2) - beta2(:,2)) ;
 % tumor parameters 3x1
 zeta_breast = nu(1).*S1(:, 1);
 zeta_prostate = nu(3).*S1(:, 2);
-% zeta ~ e+11  -- e+13 (>0)
-
-% +++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
