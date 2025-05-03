c86ec938d0019ec67e9d9126811c61c1c64f0ce0
Luisa Consiglieri <81486025+LuisaConsiglieri@users.noreply.github.com>
2025-05-03 16:56:07 +0100

    Update Zsolution.m

--- ExactSolutions/Zsolution.m
+++ ExactSolutions/Zsolution.m
@@ -5,7 +5,7 @@ function phiz = Zsolution(mu_t, eta, zz, ell, index_z)
 %   eta     - Parameter related to fluence distribution
 %   zz      - Array of z-coordinates
 %   ell     - Tumor boundary location
-%   index_z - Indices defining the range of the tumor
+%   index_z - Relevant indices
 %
 % Output:
 %   phiz    - Fluence rate as a function of z
@@ -15,6 +15,9 @@ if isempty(zz) || length(index_z) ~= 2
     error('Invalid inputs: zz must be non-empty, and index_z must have two elements.');
 end
 
+i0 = index_z(1); % Focal point
+iell = index_z(2); % Index defining the range of the tumor
+
 % Preallocate phiz
 phiz = zeros(1, length(zz)); % Preallocate for efficiency
 
@@ -23,9 +26,7 @@ if abs(zz(end) - ell) < 1e-6 % Only tumor
     phiz = exp(-mu_t * zz); % 1 x length(zz)
 else
     L = zz(end);
-    i0 = index_z(1);
-    iell = index_z(2);
-    for j = i0:length(zz) % From the focal point
+    for j = i0:length(zz) 
         if j <= iell % Tumor
             phiz(j-i0+1) = exp(-mu_t * zz(j-i0+1));
         else
