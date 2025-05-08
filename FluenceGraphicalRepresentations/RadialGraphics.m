dd2cee0189b543a1036f06c0b1c6986f83173e34
Luisa Consiglieri <81486025+LuisaConsiglieri@users.noreply.github.com>
2025-05-08 16:20:19 +0100

    Update RadialGraphics.m

--- FluenceGraphicalRepresentations/RadialGraphics.m
+++ FluenceGraphicalRepresentations/RadialGraphics.m
@@ -1,27 +1,27 @@
 % This script illustrates radial graphical representations
 
-choice = 1 % blue solid
+choice = 1; % blue solid
 [ll pw t_p] = data_operating(choice); % wavelength, power and pulse width choice
 Fluence_rate
-Xtp = exp(zeta_in*t_p) - 1;
-Rfluence = Fluence_zell(Xtp, rr, phir0, ss_phir1, z1, phiz1, indices(3:4));
-Rbreast1 = plot(rr, Rfluence(:,2), 'b');
+Xtp = exp(zeta_in*t_p);
+Rfluence = Fluence_zell(Xtp, rr, phir0, - ss_phir1, z1, phiz1, indices(3:4));
+Rbreast1 = plot(rr, Rfluence(:, d), 'b');
 hold on
 
-choice = 2 % blue dashed
+choice = 2; % blue dashed
 [ll pw t_p] = data_operating(choice); % wavelength, power and pulse width choice
 Fluence_rate
-Xtp = exp(zeta_in*t_p) - 1;
-Rfluence = Fluence_zell(Xtp, rr, phir0, ss_phir1, z1, phiz1, indices(3:4));
-Rbreast2 = plot(rr, Rfluence(:,2), 'b--');
+Xtp = exp(zeta_in*t_p);
+Rfluence = Fluence_zell(Xtp, rr, phir0, - ss_phir1, z1, phiz1, indices(3:4));
+Rbreast2 = plot(rr, Rfluence(:, d), 'b--');
 hold on
 
-choice = 3 % red dashed
+choice = 3; % red dashed
 [ll pw t_p] = data_operating(choice); % wavelength, power and pulse width choice
 Fluence_rate
-Xtp = exp(zeta_in*t_p) - 1;
-Rfluence = Fluence_zell(Xtp, rr, phir0, ss_phir1, z1, phiz1, indices(3:4));
-Rbreast3 = plot(rr, Rfluence(:,2), 'r--');
+Xtp = exp(zeta_in*t_p);
+Rfluence = Fluence_zell(Xtp, rr, phir0, - ss_phir1, z1, phiz1, indices(3:4));
+Rbreast3 = plot(rr, Rfluence(:, d), 'r--');
 hold off
 
 legStr3 = { '810 nm; 5 W', '980 nm; 5 W', '980 nm; 1.3 W' };
