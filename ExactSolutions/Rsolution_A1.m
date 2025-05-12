c386e182d7b2b263e7ff4854c338348b2cbf0154
Luisa Consiglieri <81486025+LuisaConsiglieri@users.noreply.github.com>
2025-05-12 16:05:41 +0100

    Update Rsolution_A1.m

--- ExactSolutions/Rsolution_A1.m
+++ ExactSolutions/Rsolution_A1.m
@@ -10,6 +10,10 @@ function output = Rsolution_A1(bt, Rf, rr, index_r)
 % Output: R_1 (Appendix A.1) altogether with R_2 (Appendix B).
 
 % ----------------- Input validation -------------------------------
+if length(bt) < 2
+    error('Invalid input: bt must have at least two elements.');
+end
+
 if isempty(rr) || length(index_r) ~= 2
     error('Invalid inputs: rr must be non-empty, and index_r must have two elements.');
 end
@@ -28,13 +32,22 @@ B1_out =[besselj(0, bt(2)*r_i)  besselj(1, bt(2)*r_i) ; % J0 J1 para beta2*r_i
 
 % Flux at r = r_f: Derivative = 0
 W1_in = - (pi/2)*bt(1)*r_f*Rf* B1_in(2, 2);
+% Check for Division by Zero, ensure the denominator is non-zero.
+eps = 1e-10;
+if abs(B1_in(2, 1)) < eps
+    error('Division by zero: B1_in(2, 1) is too close to zero.');
+end
 Y_in = (Rf - W1_in*B1_in(1, 1))/ B1_in(2, 1);
 
 % Continuity at r = r_i
 Ri = W1_in*besselj(0, bt(1)*r_i) + Y_in*bessely(0, bt(1)*r_i);
 DRi = - bt(1)* (W1_in*besselj(1, bt(1)*r_i) + Y_in*bessely(1, bt(1)*r_i));
 % outer Wronskian dependent parameters
-W1_out = - (pi/2)*r_i* (DRi*B1_out(2, 1) + bt(2)*Ri*B1_out(2, 2)); % ok
+W1_out = - (pi/2)*r_i* (DRi*B1_out(2, 1) + bt(2)*Ri*B1_out(2, 2));
+% Check for Division by Zero, ensure the denominator is non-zero.
+if abs(B1_out(2, 1)) < eps
+    error('Division by zero: B1_out(2, 1) is too close to zero.');
+end
 Y_out = (Ri - W1_out*B1_out(1, 1))/ B1_out(2, 1);
 
 output = zeros(1, length(rr)); % 1x100
