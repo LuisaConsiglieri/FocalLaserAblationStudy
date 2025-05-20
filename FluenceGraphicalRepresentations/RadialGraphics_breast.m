2ab47c2de199582b2d88ff2b3895dbfd69c5b7cf
Luisa Consiglieri <81486025+LuisaConsiglieri@users.noreply.github.com>
2025-05-20 14:05:55 +0100

    Add files via upload

---
+++ FluenceGraphicalRepresentations/RadialGraphics_breast.m
@@ -0,0 +1,17 @@
+% This script illustrates radial graphical representations 
+% on the breast tissue
+
+clear
+initial_data
+data_work
+data_source
+
+% Identifying the breast parameters essential to the 
+% computation of the breast fluence rate
+% for arbitrary wavelength (ll) and power (pw)
+j_tissue = 1;
+j_tumor = 1;
+j_healthy = 2;
+
+d = 2; % radial profile located at zz(d) = 0
+RadialGraphics
